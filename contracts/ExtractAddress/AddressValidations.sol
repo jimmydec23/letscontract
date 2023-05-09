aa75d6de90b8cf3b3bc68977f53356e8abce579d
jimmy <->
2023-05-09 11:49:43 +0800

    add comments

--- contracts/ExtractAddress/AddressValidations.sol
+++ contracts/ExtractAddress/AddressValidations.sol
@@ -2,12 +2,20 @@
 pragma solidity ^0.8.9;
 
 contract AddressValidations {
+    /// @param hash the data hash
+    /// @param v the recovery identifier v, range from 27 to 30
+    /// @param r x coordinate of the ECDSA curve
+    /// @param s derived from r
+    /// @return address the signer address
     function ExtractAddress(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, hash));
         return ecrecover(prefixedHash, v, r, s);
     }
 
+    /// @param hash the data hash
+    /// @param sig the signature
+    /// @return address the signer address
     function ExtractAddressWithSig(bytes32 hash, bytes memory sig) public pure returns (address) {
         if (sig.length != 65) {
             return address(0);
